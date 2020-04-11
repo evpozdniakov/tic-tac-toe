18ee5073c08f276c55eeccc9c9680427d25e4d75
evpozdniakov <evpozdniakov@gmail.com>
2020-04-11 18:52:19 +0100

    Main: remove old functions

--- main.py
+++ main.py
@@ -4,72 +4,15 @@ import model
 import position
 
 
-def trainModelScenario1():
-  n = [9, 18, 18, 18, 9]
+def trainModelScenario(n, fname, alpha = 0.001, iterations = 10000):
   (W, b) = model.initializeWeights(n)
-  alpha = 0.01
-  alphaDecayRate = 0.00001
-
-  for i in range(0, 1000):
-    trainingExamples = training.makeTrainingExamples()
-    X = trainingExamples['X']
-    Y = trainingExamples['Y']
-
-    (dW, db) = model.calcGradients(W, b, X, Y)
-
-    alpha *= 1 / (1 + alphaDecayRate * i)
-    model.updateWeights(W, dW, b, db, alpha)
-
-    if i % 100 == 0:
-      print('iteration ' + str(i))
-      AL = model.forwardPropagation(W, b, X)
-      cost = model.costFunction(Y, AL)
-      print('alpha')
-      print(alpha)
-      print('cost')
-      print(cost)
-  # '''
-  initPos = np.zeros((3, 3))
-  initPos[1][2] = -1
-
-  for i in range(0, 10):
-    print('======== initPos')
-    position.printPosition(initPos)
-    # print('initPos')
-    # print(initPos)
-
-    initPosVector = position.reshapePositionInVector(initPos)
-    # print('initPosVector')
-    # print(initPosVector)
-
-    movementVector = model.predict(W, b, initPosVector)
-    # print('nextPosVector')
-    # print(movementVector)
-
-    nextPos = (initPosVector + movementVector).reshape(3, 3)
-    position.printPosition(nextPos)
-    print('nextPos ========')
-    print('')
-
-    if position.isFinalPosition(nextPos):
-      break
-
-    initPos = position.inversePosition(nextPos)
-
-  print('------ end -------')
-  # '''
-
-
-
-def trainModelScenario2(n, fname, alpha = 0.001, iterations = 10000):
-  (W, b) = model.initializeWeights(n)
-  ex = training.readTrainingExamples(100)
+  ex = training.readTrainingExamples(24)
 
   X = ex['X']
-  assert X.shape == (9, 100)
+  # assert X.shape == (9, 500)
 
   Y = ex['Y']
-  assert Y.shape == (9, 100)
+  # assert Y.shape == (9, 500)
 
 
   # L is a number of NN layers
@@ -80,19 +23,26 @@ def trainModelScenario2(n, fname, alpha = 0.001, iterations = 10000):
 
   for i in range(0, iterations):
     # (dW, db) = model.calcGradients(W, b, X, Y)
-    (dW, db, A) = model.calcGradients2(n, W, b, X, Y)
+    (dW, db, A) = model.backPropagation(n, W, b, X, Y)
 
     model.updateWeights(W, dW, b, db, alpha)
 
     if i % 10 == 0:
       print('iteration ' + str(i))
-      # AL = model.forwardPropagation(W, b, X)
+      AL = model.forwardPropagation(W, b, X)
       # cost = model.costFunction(Y, AL)
-      cost = model.costFunction2(Y, A[L])
+      cost = model.costFunction(Y, A[L])
       # print('alpha')
       # print(alpha)
       print('cost')
       print(cost)
+
+    if i % 2 == 0:
+      is_back_prop_correct = model.checkBackPropagation(n, W, b, X, Y)
+
+      if not is_back_prop_correct:
+        print("BP is not correct")
+        exit()
 
   print('------ end -------')
 
@@ -107,51 +57,38 @@ def trainModelScenario2(n, fname, alpha = 0.001, iterations = 10000):
 
 
 def testModel(fname):
-  model9x9 = model.loadModel(fname)
+  modelInstance = model.loadModel(fname)
 
   # n = model9x9['n']
-  W = model9x9['W']
-  b = model9x9['b']
+  W = modelInstance['W']
+  b = modelInstance['b']
 
-  for _ in range(100):
-    randomPosition = position.randomPosition()
+  trainingExamples = training.readTrainingExamples(100)
 
-    position.printPosition(randomPosition)
+  for i in range(100):
+    x = trainingExamples['X'].T[i]
+    nextPosition = position.positionFromVector(x)
 
-    x = position.reshapePositionInVector(randomPosition)
+    position.printPosition(nextPosition)
 
-    print(' ')
+    print(' predicted ')
 
+    x = position.reshapePositionInVector(nextPosition)
     movement = model.predict(W, b, x)
-
     position.printMovement(movement)
+
+    print(' expected ')
+
+    y = trainingExamples['Y'].T[i]
+    position.printMovement(y.reshape(9, 1))
 
     raw_input("Press Enter to continue...")
 
 
-trainModelScenario2(n = [9, 81, 81, 9], fname = '9x81x81x9.model', alpha = 0.01, iterations=2000)
+trainModelScenario(n = [9, 9, 9, 9], fname = '9x9x9x9.model', alpha = 0.01, iterations=2000)
 
 
-# testModel('9x9x9x9.model')
-# testModel('9x18x18x9.model')
+# testModel('9x9.model')
+testModel('9x9x9x9.model')
 # testModel('9x81x81x9.model')
-
-
 # training.generateAndSaveTrainingExamples(1000)
-'''
-ex = training.readTrainingExamples()
-X = ex['X']
-Y = ex['Y']
-
-for _ in range(100):
-  randomIndex = np.floor(np.random.rand() * 1000)
-  x = X[0:9, randomIndex]
-  print('x')
-  print(x)
-  position.printPosition(x.reshape(3, 3))
-  y = Y[0:9, randomIndex]
-  print('y')
-  position.printMovement(y.reshape(9, 1))
-  raw_input("Press Enter to continue...")
-'''
-
