215f00d9766950b4099b61c24073cb327c8185da
evpozdniakov <evpozdniakov@gmail.com>
2020-05-16 20:43:37 +0100

    First unit test

---
+++ test_training.py
@@ -0,0 +1,353 @@
+import numpy as np
+import training
+import position
+
+class TestMakeSingleTrainingExampleForMainPlayer:
+  def test_non_final_player_x(self):
+    position_before = np.array([
+      0,-1, 0,
+      0, 1, 0,
+      0, 0, 0,
+    ]).reshape(3, 3)
+
+    result_position = np.array([
+      1,-1, 0,
+      0, 1, 0,
+      0, 0, 0,
+    ]).reshape(3, 3)
+
+    movement = {
+      'coords': (0, 0),
+      'highest_al': 0.5,
+      'result_position': result_position,
+    }
+
+    final_position = np.array([
+      1,-1, 0,
+      0, 1,-1,
+      0, 0, 1,
+    ]).reshape(3, 3)
+    (x, y) = training.make_single_training_example_for_main_player(position_before, movement, final_position)
+
+    expected_x = np.array([
+      0,-1, 0,
+      0, 1, 0,
+      0, 0, 0,
+    ]).reshape(9, 1)
+
+    # test x
+    assert (x == expected_x).all()
+
+    expected_y = np.array([
+      0.5, 0.001, 0,
+      0, 0.001, 0,
+      0, 0, 0,
+    ]).reshape(9, 1)
+
+    # test y
+    assert (y == expected_y).all()
+
+  def test_non_final_player_o(self):
+    position_before = np.array([
+      1,-1, 0,
+      0, 1, 0,
+      0, 0, 0,
+    ]).reshape(3, 3)
+
+    result_position = np.array([
+      1,-1, 0,
+      0, 1, 0,
+      0, 0,-1,
+    ]).reshape(3, 3)
+
+    movement = {
+      'coords': (2, 2),
+      'highest_al': 0.3,
+      'result_position': result_position,
+    }
+
+    final_position = np.array([
+      1,-1, 0,
+      1, 1,-1,
+      1, 0,-1,
+    ]).reshape(3, 3)
+
+    (x, y) = training.make_single_training_example_for_main_player(position_before, movement, final_position)
+
+    expected_x = np.array([
+     -1, 1, 0,
+      0,-1, 0,
+      0, 0, 0,
+    ]).reshape(9, 1)
+
+    # test x
+    assert (x == expected_x).all()
+
+    expected_y = np.array([
+      0.001, 0.001, 0,
+      0, 0.001, 0,
+      0, 0, 0.3,
+    ]).reshape(9, 1)
+
+    # test y
+    assert (y == expected_y).all()
+
+  def test_player_x_wins(self):
+    position_before = np.array([
+      1,-1, 0,
+      0, 1,-1,
+      0, 0, 0,
+    ]).reshape(3, 3)
+
+    result_position = np.array([
+      1,-1, 0,
+      0, 1,-1,
+      0, 0, 1,
+    ]).reshape(3, 3)
+
+    movement = {
+      'coords': (2, 2),
+      'highest_al': 0.7,
+      'result_position': result_position,
+    }
+
+    final_position = position.clone(result_position)
+
+    (x, y) = training.make_single_training_example_for_main_player(position_before, movement, final_position)
+
+    expected_x = np.array([
+      1,-1, 0,
+      0, 1,-1,
+      0, 0, 0,
+    ]).reshape(9, 1)
+
+    # test x
+    assert (x == expected_x).all()
+
+    expected_y = np.array([
+      0.001, 0.001, 0,
+      0, 0.001, 0.001,
+      0, 0, 1,
+    ]).reshape(9, 1)
+
+    # test y
+    assert (y == expected_y).all()
+
+  def test_player_x_makes_draw(self):
+    position_before = np.array([
+      1,-1, 1,
+     -1, 1, 0,
+     -1, 1,-1,
+    ]).reshape(3, 3)
+
+    result_position = np.array([
+      1,-1, 1,
+     -1, 1, 1,
+     -1, 1,-1,
+    ]).reshape(3, 3)
+
+    movement = {
+      'coords': (1, 2),
+      'highest_al': 0.4,
+      'result_position': result_position,
+    }
+
+    final_position = position.clone(result_position)
+
+    (x, y) = training.make_single_training_example_for_main_player(position_before, movement, final_position)
+
+    expected_x = np.array([
+      1,-1, 1,
+     -1, 1, 0,
+     -1, 1,-1,
+    ]).reshape(9, 1)
+
+    # test x
+    assert (x == expected_x).all()
+
+    expected_y = np.array([
+      0.001, 0.001, 0.001,
+      0.001, 0.001, 0.5,
+      0.001, 0.001, 0.001,
+    ]).reshape(9, 1)
+
+    # test y
+    assert (y == expected_y).all()
+
+  def test_player_x_looses(self):
+    position_before = np.array([
+      1, 0,-1,
+     -1, 1, 0,
+      0, 1,-1,
+    ]).reshape(3, 3)
+
+    result_position = np.array([
+      1, 0,-1,
+     -1, 1, 0,
+      1, 1,-1,
+    ]).reshape(3, 3)
+
+    movement = {
+      'coords': (2, 0),
+      'highest_al': 0.2,
+      'result_position': result_position,
+    }
+
+    final_position = np.array([
+      1, 0,-1,
+     -1, 1,-1,
+      1, 1,-1,
+    ]).reshape(3, 3)
+
+    (x, y) = training.make_single_training_example_for_main_player(position_before, movement, final_position)
+
+    expected_x = np.array([
+      1, 0,-1,
+     -1, 1, 0,
+      0, 1,-1,
+    ]).reshape(9, 1)
+
+    # test x
+    assert (x == expected_x).all()
+
+    expected_y = np.array([
+      0.001, 0, 0.001,
+      0.001, 0.001, 0,
+      0.1, 0.001, 0.001,
+    ]).reshape(9, 1)
+
+    # test y
+    assert (y == expected_y).all()
+
+  def test_player_o_wins(self):
+    position_before = np.array([
+      1, 0,-1,
+     -1, 1, 0,
+      1, 1,-1,
+    ]).reshape(3, 3)
+
+    result_position = np.array([
+      1, 0,-1,
+     -1, 1,-1,
+      1, 1,-1,
+    ]).reshape(3, 3)
+
+    movement = {
+      'coords': (1, 2),
+      'highest_al': 0.7,
+      'result_position': result_position,
+    }
+
+    final_position = position.clone(result_position)
+
+    (x, y) = training.make_single_training_example_for_main_player(position_before, movement, final_position)
+
+    expected_x = np.array([
+     -1, 0, 1,
+      1,-1, 0,
+     -1,-1, 1,
+    ]).reshape(9, 1)
+
+    # test x
+    assert (x == expected_x).all()
+
+    expected_y = np.array([
+      0.001, 0, 0.001,
+      0.001, 0.001, 1,
+      0.001, 0.001, 0.001,
+    ]).reshape(9, 1)
+
+    # test y
+    assert (y == expected_y).all()
+
+  def test_player_o_makes_draw(self):
+    position_before = np.array([
+      1, 0, 1,
+     -1, 1, 0,
+     -1, 1,-1,
+    ]).reshape(3, 3)
+
+    result_position = np.array([
+      1,-1, 1,
+     -1, 1, 0,
+     -1, 1,-1,
+    ]).reshape(3, 3)
+
+    movement = {
+      'coords': (0, 1),
+      'highest_al': 0.4,
+      'result_position': result_position,
+    }
+
+    # final_position = position.clone(result_position)
+    final_position = np.array([
+      1,-1, 1,
+     -1, 1, 1,
+     -1, 1,-1,
+    ]).reshape(3, 3)
+
+    (x, y) = training.make_single_training_example_for_main_player(position_before, movement, final_position)
+
+    expected_x = np.array([
+     -1, 0,-1,
+      1,-1, 0,
+      1,-1, 1,
+    ]).reshape(9, 1)
+
+    # test x
+    assert (x == expected_x).all()
+
+    expected_y = np.array([
+      0.001, 0.5, 0.001,
+      0.001, 0.001, 0,
+      0.001, 0.001, 0.001,
+    ]).reshape(9, 1)
+
+    # test y
+    assert (y == expected_y).all()
+
+  def test_player_o_looses(self):
+    position_before = np.array([
+      1,-1, 0,
+      0, 1, 0,
+      0, 0, 0,
+    ]).reshape(3, 3)
+
+    result_position = np.array([
+      1,-1, 0,
+     -1, 1, 0,
+      0, 0, 0,
+    ]).reshape(3, 3)
+
+    movement = {
+      'coords': (1, 0),
+      'highest_al': 0.4,
+      'result_position': result_position,
+    }
+
+    # final_position = position.clone(result_position)
+    final_position = np.array([
+      1,-1, 0,
+     -1, 1, 0,
+      0, 0, 1,
+    ]).reshape(3, 3)
+
+    (x, y) = training.make_single_training_example_for_main_player(position_before, movement, final_position)
+
+    expected_x = np.array([
+     -1, 1, 0,
+      0,-1, 0,
+      0, 0, 0,
+    ]).reshape(9, 1)
+
+    # test x
+    assert (x == expected_x).all()
+
+    expected_y = np.array([
+      0.001, 0.001, 0,
+      0.1, 0.001, 0,
+      0, 0, 0,
+    ]).reshape(9, 1)
+
+    # test y
+    assert (y == expected_y).all()
